e84d4e593a42f035f44f44304432737ca890396b
Yurov Ruslan <yurovrusik@gmail.com>
2016-12-18 17:06:54 +0300

    простейший способ получения страницы в строку

--- sona/Program.cs
+++ sona/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
+using System.Net;
+
+using Encoding = System.Text.Encoding;
 
 namespace sona
 {
 	class MainClass
 	{
+		public static readonly string[] sites = {"http://jitcs.ru",
+			"http://www.jit.nsu.ru", "http://sv-journal.org",
+			"http://www.novtex.ru", "http://aidt.ru",
+			"http://ipiran.ru", "http://ubs.mtas.ru",
+			"http://bijournal.hse.ru", "http://www.isa.ru"};
+		
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			WebClient client = new WebClient ();
+			client.Encoding = Encoding.GetEncoding("windows-1251");
+			string reply = client.DownloadString (sites[6]);
 		}
 	}
 }
