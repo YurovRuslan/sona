5aa126f1f2586508d27067567a41881fc1648759
Yurov Ruslan <yurovrusik@gmail.com>
2016-12-24 20:02:35 +0300

    добавлена библиотека System.Xml.Xpath, которой так не хватает либе HtmlAgilityPack

--- sona/Program.cs
+++ sona/Program.cs
@@ -9,17 +9,21 @@ namespace sona
 {
 	class MainClass
 	{
-		public static readonly string[] sites = {"http://jitcs.ru",
-			"http://www.jit.nsu.ru", "http://sv-journal.org",
-			"http://www.novtex.ru", "http://aidt.ru",
-			"http://ipiran.ru", "http://ubs.mtas.ru",
-			"http://bijournal.hse.ru", "http://www.isa.ru"};
+		public static readonly string[] sites =
+			{"http://jitcs.ru",
+			"http://www.jit.nsu.ru",
+			"http://sv-journal.org",
+			"http://www.novtex.ru",
+			"http://aidt.ru",
+			"http://ipiran.ru",
+			"http://ubs.mtas.ru/archive",
+			"http://bijournal.hse.ru",
+			"http://www.isa.ru"};
 		
 		public static void Main (string[] args)
 		{
 			WebClient client = new WebClient ();
 			client.Encoding = Encoding.GetEncoding("windows-1251");
-			string reply = client.DownloadString (sites[6]);
 			var html_node = new HtmlDocument ();
 			html_node.LoadHtml (client.DownloadString (sites[6]));
 		}
