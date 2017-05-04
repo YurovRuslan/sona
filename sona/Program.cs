38ca5d8c6b95b53e386380f4d20239aa051d6146
yurov <iurovrs@gmail.com>
2017-05-04 23:26:39 +0300

    рефакторинг
    - все переведено на классы
    - использование паттерна Фабрика
    - добавлен bijournal.hse.ru

--- sona/Program.cs
+++ sona/Program.cs
@@ -16,6 +16,7 @@ using Encoding = System.Text.Encoding;
 
 namespace sona
 {
+	using Date = Tuple<int, int>;
 	class MainClass
 	{
 		public static readonly string[] sites =
@@ -31,6 +32,397 @@ namespace sona
 			"http://www.isa.ru/"		// WIP: looking only last magazine issue for | /proceedings
 		};
 
+		public enum WebResource {
+			JITCS,
+			JITNSU,
+			SVJOURNAL,
+			NOVTEX,
+			AIDT,
+			IPIRAN,
+			UBSMTAS,
+			BIJOURNAL,
+			ISA
+		};
+
+		public class Issue {
+			private int number;
+			public int Number { get; set; }
+
+			private int year;
+			public int Year { get; set; }
+		};
+
+		public interface ISource {
+			void Parse();
+		}
+
+		public class Jitcs : ISource {
+			public Jitcs() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				htmlNode.LoadHtml (client.DownloadString (url));
+				var documentNode = htmlNode.DocumentNode;
+				var trNode = documentNode
+					.SelectNodes ("//tr[@class='leftmenuarticles']") [0]
+					.SelectSingleNode ("td/div");
+				var aNode = trNode.SelectSingleNode ("a");
+				date = Tuple.Create(Convert.ToInt32 (aNode.InnerText.Replace (" / ", " ").Split (' ') [0]),
+						Convert.ToInt32 (aNode.InnerText.Replace(" / ", " ").Split(' ')[1]));
+				var link = aNode.Attributes ["href"] != null
+					? HttpUtility.HtmlDecode (aNode.Attributes ["href"].Value.ToString ())
+					: "Can't parse";
+				htmlNode.LoadHtml (client.DownloadString (link));
+				documentNode = htmlNode.DocumentNode;
+				var trArtNodes = documentNode
+					.SelectNodes ("//tr[@class='leftmenuarticles']");
+				List<string> articlesList = new List<string>();
+				articlesList.Add (link);
+				foreach (var trArtNode in trArtNodes.Skip(1)) {
+					var aArtNodes = trArtNode
+						.SelectNodes ("td/div/a");
+					foreach (var aArtNode in aArtNodes) {
+						articlesList.Add(aArtNode.Attributes ["href"] != null
+							? HttpUtility.HtmlDecode (aArtNode.Attributes ["href"].Value.ToString ())
+							: "Can't parse");
+					}
+				}
+				articles = articlesList.ToArray();
+			}
+
+			private string url = "http://jitcs.ru/";
+			private Date date;
+			private Array articles;
+		}
+
+		public class JitNsu : ISource {
+			public JitNsu() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				htmlNode.LoadHtml (client.DownloadString (url));
+				var documentNode = htmlNode.DocumentNode;
+				var lastIssue = documentNode
+					.SelectNodes ("//dd/a")
+					.Where (node => node.InnerText == "Последний выпуск")
+					.Select(node => sites[1] + node.Attributes["href"].Value.ToString())
+					.First();
+				htmlNode.LoadHtml (client.DownloadString (lastIssue));
+				documentNode = htmlNode.DocumentNode;
+				date = Tuple.Create(Convert.ToInt32(documentNode
+						.SelectNodes("//body/p")[0]
+						.InnerText
+						.Split('№')[1]
+						.Replace("(", string.Empty)
+						.Replace(")", string.Empty)
+						.Substring(1)
+						.Split(' ')[0]),
+					Convert.ToInt32(documentNode
+						.SelectNodes("//body/p")[0]
+						.InnerText
+						.Split('№')[1]
+						.Replace("(", string.Empty)
+						.Replace(")", string.Empty)
+						.Substring(1)
+						.Split(' ')[1]));
+				var aNode = documentNode
+					.SelectNodes ("//dd/a");
+				articles = aNode
+					.Take(aNode.Count - 2)
+					.Select(node => node.Attributes ["href"] != null
+						? HttpUtility.HtmlDecode (sites[1] + node.Attributes ["href"].Value.ToString ())
+						: "Can't parse")
+					.ToArray();
+			}
+
+			private string url = "http://jit.nsu.ru/index.php?+ru";
+			private Date date;
+			private Array articles;
+		}
+
+		public class Isa : ISource {
+			public Isa() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				htmlNode.LoadHtml (client.DownloadString (url));
+				var documentNode = htmlNode.DocumentNode;
+				var trNode = documentNode
+					.SelectNodes ("//tr[@class='leftmenuarticles']") [0]
+					.SelectSingleNode ("td/div");
+				var aNode = trNode.SelectSingleNode ("a");
+				date = Tuple.Create(Convert.ToInt32 (aNode.InnerText.Split (new Char [] {' ', '-'}).ToList()[1]), -1);
+				var link = aNode.Attributes ["href"] != null
+					? HttpUtility.HtmlDecode (aNode.Attributes ["href"].Value.ToString ())
+					: "Can't parse";
+				htmlNode.LoadHtml (client.DownloadString (link));
+				documentNode = htmlNode.DocumentNode;
+				var trArtNodes = documentNode
+					.SelectNodes ("//tr[@class='leftmenuarticles']");
+				List<string> articlesList = new List<string>();
+				articlesList.Add (link);
+				foreach (var trArtNode in trArtNodes.Skip(1)) {
+					var aArtNodes = trArtNode
+						.SelectNodes ("td/div/a");
+					foreach (var aArtNode in aArtNodes) {
+						articlesList.Add(aArtNode.Attributes ["href"] != null
+							? HttpUtility.HtmlDecode (aArtNode.Attributes ["href"].Value.ToString ())
+							: "Can't parse");
+					}
+				}
+				articles = articlesList.ToArray ();
+			}
+
+			private string url = "http://isa.ru/proceedings";
+			private Date date;
+			private Array articles;
+		}
+
+		public class UbsMtas : ISource {
+			public UbsMtas() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				while (!String.IsNullOrEmpty (url)) {
+					htmlNode.LoadHtml (client.DownloadString (url));
+					var documentNode = htmlNode.DocumentNode;
+					try {
+						var ulNode = documentNode
+							.SelectNodes ("//ul")
+							.Select (node => node.LastChild);
+						var aNode = ulNode.ElementAt (0).SelectSingleNode ("a");
+						var linkNode = aNode.Attributes ["href"] != null
+							? HttpUtility.HtmlDecode (sites [6] + aNode.Attributes ["href"].Value.ToString ())
+							: "Can't parse";
+						date = Tuple.Create(Convert.ToInt32 (aNode.InnerText.Split (new Char [] {' ', '&'}).ToList()[1]), -1);
+						url = linkNode;
+					} catch (ArgumentNullException) {
+						var articlesUrl = documentNode
+							.SelectNodes ("//td[not(@*)]/a")
+							.Select (node => node.Attributes ["href"].Value != null
+								? HttpUtility.HtmlDecode (sites [6] + node.Attributes ["href"].Value)
+								: "Can't parse")
+							.ToArray ();
+						articles = articlesUrl;
+					}
+				}
+				articles = new string[] {};
+			}
+
+			private string url = "http://ubs.mtas.ru/archive";
+			private Date date;
+			private Array articles;
+		}
+
+		public class SvJournal : ISource {
+			public SvJournal() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				htmlNode.LoadHtml (client.DownloadString (url));
+				var documentNode = htmlNode.DocumentNode;
+				var lastIssue = documentNode
+					.SelectNodes ("//tr[2]/td[@class='nr' and last()]/a")
+					.Select (node => node.Attributes ["href"] != null
+						? HttpUtility.HtmlDecode (url + node.Attributes ["href"].Value.ToString ())
+						: "Can't parse").ElementAt(0);
+				var uriAddress = new Uri (lastIssue);
+				var issueDate = uriAddress.AbsolutePath.Split('/')[1] + '/';
+				htmlNode.LoadHtml (client.DownloadString (lastIssue));
+				documentNode = htmlNode.DocumentNode;
+				var articlesArray = documentNode
+					.SelectNodes ("//tr/td[@class='pub_pp']/a")
+					.Select (node => node.Attributes ["href"] != null
+						? HttpUtility.HtmlDecode (url + issueDate + node.Attributes ["href"].Value.ToString ())
+						: "Can't parse")
+					.ToArray();
+				date = Tuple.Create(Convert.ToInt32 (articlesArray [0]
+						.Split ('/') [3]
+						.Replace ('-', ' ').Split(' ')[0]),
+					Convert.ToInt32 (articlesArray [0]
+						.Split ('/') [3]
+						.Replace ('-', ' ').Split(' ')[1]));
+				articles = articlesArray;
+			}
+
+			private string url = "http://sv-journal.org/issues.php?lang=ru";
+			private Date date;
+			private Array articles;
+		}
+
+		public class Aidt : ISource {
+			public Aidt() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				htmlNode.LoadHtml (client.DownloadString (url));
+				var documentNode = htmlNode.DocumentNode;
+				var lastIssue = url + documentNode.SelectSingleNode ("//div[@id='avatar-right']//li[1]/a")
+					.GetAttributeValue("href", "Can't parse");
+				htmlNode.LoadHtml (client.DownloadString (lastIssue));
+				documentNode = htmlNode.DocumentNode;
+				date = Tuple.Create(Convert.ToInt32 (documentNode
+						.SelectNodes ("//div[@class='category-list']/h2/span") [0]
+						.InnerText
+						.Replace (" / ", " ").Split(' ')[0]),
+					Convert.ToInt32 (documentNode
+						.SelectNodes ("//div[@class='category-list']/h2/span") [0]
+						.InnerText
+						.Replace (" / ", " ").Split(' ')[1]));
+				articles = documentNode
+					.SelectNodes ("//div[@class='sectionlist']/ul/li/a")
+					.Select( node => node.Attributes ["href"] != null
+						? HttpUtility.HtmlDecode (url + node.Attributes ["href"].Value.ToString ())
+						: "Can't parse")
+					.ToArray();
+			}
+
+			private string url = "http://aidt.ru/index.php?lang=ru";
+			private Date date;
+			private Array articles;
+		}
+
+		public class BiJournal : ISource {
+			public BiJournal() { }
+
+			public int Issue { get; set; }
+
+			public void Parse() {
+				WebClient client = new WebClient ();
+				client.Encoding = Encoding.GetEncoding (
+					SearchEnc.SearchEncoding(url));
+				var htmlNode = new HtmlDocument ();
+				htmlNode.LoadHtml (client.DownloadString (url));
+				var documentNode = htmlNode.DocumentNode;
+				var lastIssue = documentNode.SelectSingleNode ("//div[@class='journal']//div[@class='new-num']/a")
+					.GetAttributeValue("href", "Can't parse");
+				htmlNode.LoadHtml (client.DownloadString (lastIssue));
+				documentNode = htmlNode.DocumentNode;
+				date = Tuple.Create(Convert.ToInt32 (new Uri(lastIssue).AbsolutePath.Substring(1).Split('-')[0]),
+					Convert.ToInt32 (new Uri(lastIssue).AbsolutePath.Substring(1).Split('-')[2].Split('%')[0]));
+				articles = documentNode
+					.SelectNodes ("//table[@class='link']//div[@class='link']/a")
+					.Select( node => node.Attributes ["href"] != null
+						? HttpUtility.HtmlDecode (node.Attributes ["href"].Value.ToString ())
+						: "Can't parse")
+					.ToArray();
+			}
+
+			private string url = "http://bijournal.hse.ru";
+			private Date date;
+			private Array articles;
+		}
+
+		public class Factory {
+			public ISource CreateSource (WebResource resource)
+			{
+				switch (resource)
+				{
+				case WebResource.JITCS:
+					return new Jitcs();
+				case WebResource.JITNSU:
+					return new JitNsu();
+				case WebResource.AIDT:
+					return new Aidt();
+				case WebResource.SVJOURNAL:
+					return new SvJournal();
+				case WebResource.UBSMTAS:
+					return new UbsMtas();
+				case WebResource.BIJOURNAL:
+					return new BiJournal();
+				case WebResource.IPIRAN:
+					throw new NotImplementedException ("Method no implemented. Please try again later.");
+				default:
+					throw new ArgumentOutOfRangeException();
+				}
+			}
+		}
+
+		public class Journal {
+			public Journal(WebResource resource)
+			{
+				_name = resource;
+			}
+
+			public Journal(WebResource resource, Issue issue)
+			{
+				_name = resource;
+				_issue.Number = issue.Number;
+				_issue.Year = issue.Year;
+			}
+
+			public WebResource Name { get; set; }
+			public int Issue { get; set; }
+
+			public int Number() { return _issue.Number; }
+			public void Number(int number) { _issue.Number = number; }
+
+			public int Year() { return _issue.Year; }
+			public void Year(int year) { _issue.Year = year; }
+
+			//public Array Parse ();
+
+			public override string ToString()
+			{
+				switch (_name)
+				{
+					case WebResource.JITCS:
+						return "jitcs";
+					case WebResource.JITNSU:
+						return "jitnsu";
+					case WebResource.AIDT:
+						return "aidt";
+					case WebResource.BIJOURNAL:
+						return "bijournal";
+					case WebResource.IPIRAN:
+						return "ipiran";
+					case WebResource.ISA:
+						return "isa";
+					case WebResource.NOVTEX:
+						return "novtex";
+					case WebResource.SVJOURNAL:
+						return "svJournal";
+					case WebResource.UBSMTAS:
+						return "ubsMtas";
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+			}
+
+			private Issue _issue;
+			private WebResource _name;
+		};
+
 		public static int Main (string[] args)
 		{
 			/*if (args.Length > 2) {
@@ -39,20 +431,49 @@ namespace sona
 			} else if (args.Length < 1) {
 				Console.WriteLine ("Too few command line args.");
 				return -1;
-			}*
-			var journal = args[0];
-			string directArticleLink = args[1];*/
-			var journal = "aidt";
-			string directArticleLink = "";
+			}
+			var journal = args[0];*/
+			var journal = "http://bijournal.hse.ru";
 
-			string startingPoint = sites [1];
-			WebClient client = new WebClient ();
-			client.Encoding = Encoding.GetEncoding (
-				SearchEnc.SearchEncoding(startingPoint));
-			Array links;
-			string number = "";
-			int month = 0;
-			int year = 0;
+			Factory factory = new Factory ();
+			var source = factory.CreateSource (toWebResource(journal));
+
+			var lastIssueDate = getDateFromDb (journal);
+			source.Parse ();
+
+			/*foreach(var item in source) {
+				Console.WriteLine(item.ToString());
+			}*/
+			return 0;
+		}
+
+		public static WebResource toWebResource(string url) {
+			Uri uri = new Uri (url);
+			switch (uri.Host)
+			{
+				case "jitcs.ru":
+					return WebResource.JITCS;
+				case "jit.nsu.ru":
+					return WebResource.JITNSU;
+				case "aidt.ru":
+					return WebResource.AIDT;
+				case "sv-journal.org":
+					return WebResource.SVJOURNAL;
+				case "ipiran.ru":
+					return WebResource.IPIRAN;
+					case "isa.ru":
+					return WebResource.ISA;
+				case "ubs.mtas.ru":
+					return WebResource.UBSMTAS;
+				case "bijournal.hse.ru":
+					return WebResource.BIJOURNAL;
+				default:
+					throw new ArgumentOutOfRangeException();
+			}
+		}
+
+		public static Tuple<int, int> getDateFromDb(string journal) {
+			return Tuple.Create(1, 2017);
 			string cs = @"server=localhost;userid=sona;password=123456;database=macDuck";
 			MySqlConnection connection = null;
 			try {
@@ -60,7 +481,7 @@ namespace sona
 				connection.Open();
 				var cmd = connection.CreateCommand();
 				cmd.CommandText = "select id from journals where name = @name";
-				cmd.Parameters.AddWithValue("@name", journal);
+				cmd.Parameters.AddWithValue("@name", journal.ToString());
 				string id = "";
 				using (var reader = cmd.ExecuteReader()) {
 					if (reader.Read()) {
@@ -71,71 +492,17 @@ namespace sona
 				cmd.Parameters.AddWithValue("@journal", id);
 				using (var reader = cmd.ExecuteReader()) {
 					if (reader.Read()) {
-						month = Convert.ToInt32(reader[0]);
-						year = Convert.ToInt32(reader[1]);
+						if (connection != null) {
+							connection.Close();
+						}
+						return Tuple.Create(Convert.ToInt32(reader[0]), Convert.ToInt32(reader[1]));
 					}
 				}
+				return Tuple.Create(-1, -1);
 			} catch (MySqlException ex)	{
 				Console.WriteLine("Error: {0}",  ex.ToString());
-			} finally {          
-				if (connection != null) {
-					connection.Close();
-				}
+				return Tuple.Create(-1, -1);
 			}
-
-			if (journal == "jitnsu") {
-				links = jitnsuParser (sites [1] + "index.php?+ru", client, ref number, directArticleLink);
-				if (month == Convert.ToInt32(number.Split (' ') [0]) || year == Convert.ToInt32(number.Split (' ') [1])) {
-					return -1;
-				}
-				File.WriteAllText("jitnsu.txt", number);
-			} else if (journal == "jitcs") {
-				links = jitcsParser (sites[0], client, ref number, directArticleLink);
-				if (month == Convert.ToInt32(number.Split (' ') [0]) || year == Convert.ToInt32(number.Split (' ') [1])) {
-					return -1;
-				}
-				File.WriteAllText("jitcs.txt", number);
-			} else if (journal == "isa") {
-				links = isaParser (sites[8] + "proceedings", client, ref number, directArticleLink);
-				if (month == Convert.ToInt32(number)) {
-					return -1;
-				}
-				File.WriteAllText("isa.txt", number);
-			} else if (journal == "ubsMtas") {
-				links = ubsMtasParser (sites[6] + "archive", client, ref number, directArticleLink);
-				if (month == Convert.ToInt32(number)) {
-					return -1;
-				}
-				File.WriteAllText("ubsMtas.txt", number);
-			} else if (journal == "svJournal") {
-				links = svJournalParser (sites[2], client, ref number, directArticleLink);
-				if (month == Convert.ToInt32(number.Split (' ') [0]) || year == Convert.ToInt32(number.Split (' ') [1])) {
-					return -1;
-				}
-				File.WriteAllText("svJournal.txt", number);
-			/*} else if (journal == "novtex") {
-				links = novtexParser (sites[3], client);
-				File.WriteAllText("novtex.txt", createText);*/
-			} else {
-				links = aidtParser(sites[4], client, ref number, directArticleLink);
-				if (month == Convert.ToInt32(number.Split (' ') [0]) || year == Convert.ToInt32(number.Split (' ') [1])) {
-					return -1;
-				}
-				File.WriteAllText("aidt.txt", number);
-			}
-			foreach(var item in links) {
-				Console.WriteLine(item.ToString());
-			}
-			return 0;
-		}
-
-
-		/*
-		 * SqlWriter
-		 * /
-
-		public static bool sqlWriter() {
-
 		}
 
 
